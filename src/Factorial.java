e55fec4f089335f800a35d2dbb3f7ffb0f159fec
drushing <drushing.dev@gmail.com>
2014-11-05 14:32:23 -0600

    added some notes

---
+++ src/Factorial.java
@@ -0,0 +1,31 @@
+/* Factorial -- an iterative and recursive method for finding the factorial of a number
+ * -- only using int datatypes
+ * */
+public class Factorial {
+
+	private int value;
+	private int factorialValue;
+	
+	public Factorial(int x){
+		this.value = x;
+	}
+
+	/*  */
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public int getFactorialValue() {
+		return factorialValue;
+	}
+
+	public void setFactorialValue(int factorialValue) {
+		this.factorialValue = factorialValue;
+	}
+	
+	
+}
